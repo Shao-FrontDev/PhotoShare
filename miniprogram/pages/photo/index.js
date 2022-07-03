eefdd88609fde62099a689610108822707a1853c
v_deyushao <v_deyushao@tencent.com>
2022-07-03 22:25:52 +0800

    feat: 完善了photo的基础样式调整

---
+++ miniprogram/pages/photo/index.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    background: [
+      "demo-text-1",
+      "demo-text-2",
+      "demo-text-3",
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 100,
+  },
+});
